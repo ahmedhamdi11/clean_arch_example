23a0489aa511c73a356de3305f7a8b28a30fe11f
Ahmed Hamdi <ahmedhamdi725@gmail.com>
2024-08-02 15:47:55 +0300

    get weather by city name use case created

---
+++ lib/weather/domain/usecases/get_weather_by_city_name.dart
@@ -0,0 +1,13 @@
+import 'package:clean_arch_example/weather/domain/entities/weather_entity.dart';
+import 'package:clean_arch_example/weather/domain/repository/weather_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class GetWeatherByCityName {
+  final WeatherRepo _repo;
+
+  const GetWeatherByCityName(this._repo);
+
+  Future<Either<String, WeatherEntity>> execute(String cityName) async {
+    return await _repo.getWeatherByCityName(cityName);
+  }
+}
